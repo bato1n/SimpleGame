b75096ad5870678c93ccc8af834fc4d70bf655b9
bato1n <bato1n@o2.pl>
2016-11-01 09:46:30 +0100

    Add files via upload

---
+++ CountLine/src/countline/CountLine.java
@@ -0,0 +1,40 @@
+package countline;
+
+import java.io.*;
+
+public class CountLine 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        System.out.println(countLines("C:\\test.txt")); 
+    }
+   public static int countLines(String filename) throws IOException
+    {
+        InputStream is = new BufferedInputStream(new FileInputStream(filename));
+        try
+        {
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars = 0;
+            boolean empty = true;
+            while ((readChars = is.read(c)) != -1)
+            {
+                if(empty) count++;
+                empty = false;
+                for (int i = 0; i < readChars; ++i)
+                {
+                    if (c[i] == '\n')
+                    {
+                        ++count;
+                    }
+                }
+            }
+            return (count == 0 && !empty) ? 1 : count;
+        }
+        finally
+        {
+            is.close();
+        }
+    }
+    
+}
